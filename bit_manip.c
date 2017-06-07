45acea10f98d7bd04281533f407905291ff00769
vasistagithub <vasista.cse@gmail.com>
2017-06-07 12:00:12 +0530

    updating bit_manip.c

--- bit_manip.c
+++ bit_manip.c
@@ -6,14 +6,17 @@ int get_endian();
 int main()
 {
     int endianness = 0; /*Default is big-endian assumed*/
-    
+    int num = 0;
+
     endianness = get_endian();
     if(endianness) {
         printf("Little Endian\n");
     } else {
         printf("Big Endian\n");
     }
-
+    printf("Enter number to be reversed byte order way:");
+    scanf("%d", &num);
+    printf("%d after byte reversal is %d\n", num, reverse_byte_by_byte(num));
     return 0;
 }
 
@@ -26,4 +29,19 @@ int get_endian()
     printf("value in ptr is %d\n", (*ptr));
 
     return (*ptr);
+}
+
+int reverse_byte_by_byte(int num)
+{
+    unsigned int c1, c2, c3, c4;
+    int final = 0;
+
+    c1 = num & 255;
+    c2 = (num >> 8) & 255;
+    c3 = (num >> 16) & 255;
+    c4 = (num >> 24) & 255;
+
+    final = (c1 << 24) | (c2 << 16) | (c3 << 8) | c4;
+
+    return final;
 }
