d6131f8001df7162e80553e94fc0c35aa6c5bdb2
vasistagithub <vasista.cse@gmail.com>
2017-07-18 07:54:12 +0530

    updated graph.c file

--- graph.c
+++ graph.c
@@ -8,12 +8,21 @@
 
 struct node
 {
-    int data;
-    struct node* link;
+    int vertex;
+    struct node* next;
 }**head = NULL;
 
+struct node* front = NULL;
+struct node* rear = NULL;
+struct node* top = NULL;
+
+int *parent = NULL;
+int *color = NULL;
+ 
 void insert_graph(struct node**,int);
 void display_graph(struct node**,int);
+void BFS(struct node**, int);
+void DFS(struct node**, int);
 
 int main()
 {
@@ -21,17 +30,12 @@ int main()
     int i;
     int adj_node;
 
-    printf("\n\n\t\t ADJACENCY LIST\n\n");
+    printf("\n\n\t\t GRAPH OPERATIONS\n\n");
+    printf("Represent Graph in Adjacency list\n");
     printf("Enter the number of nodes::");
     scanf("%d",&num_nodes);
 
     head = (struct node**)malloc(num_nodes * sizeof(struct node*));
-
-    for(i=0; i<num_nodes; i++) {
-        head[i] = (struct node*)calloc(sizeof(struct node),1);
-        head[i]->data = i;
-        head[i]->link = NULL;
-    }
 
     for(i=0; i<num_nodes; i++) {
         while(1) {
@@ -44,9 +48,15 @@ int main()
             }
         }
     }
-
+ 
     printf("\nDisplaying graph in adjacency list format\n\n");
     display_graph(head, num_nodes);
+
+    printf("\n Performing BFS on Graph\n");
+    BFS(head, num_nodes);
+    
+    printf("\n Performing DFS on Graph\n");
+    DFS(head, num_nodes);
 
     return 0;
 }
@@ -57,15 +67,19 @@ void insert_graph(struct node** head, int graph_node)
     struct node* temp_head;
     
     temp = (struct node*)malloc(sizeof(struct node));
-    temp->data = graph_node;
-    temp->link = NULL;
+    temp->vertex = graph_node;
+    temp->next = NULL;
 
     temp_head = *head;
-    
-    while(temp_head->link) {
-        temp_head =temp_head->link;
+
+    if(temp_head == NULL) {
+        *head = temp;
+    } else {    
+       while(temp_head->next) {
+           temp_head = temp_head->next;
+       }
+        temp_head->next = temp;
     }
-    temp_head->link = temp;
 }
 
 
@@ -77,10 +91,146 @@ void display_graph(struct node **head, int num_nodes)
     for(i=0; i<num_nodes; i++) {
         temp = head[i];
         while(temp) {
-            printf("%d---",temp->data);
-            temp = temp->link;
+            printf("%d---",temp->vertex);
+            temp = temp->next;
         }
         printf("\n\n");
     }
 }
 
+void enqueue(int vertex)
+{
+    struct node* tmp = NULL;
+    tmp = (struct node*)malloc(sizeof(struct node));
+    tmp->vertex = vertex;
+    tmp->next = NULL;
+
+    if(front == NULL) {
+        front =rear = tmp;
+    } else {
+        rear->next = tmp;
+        rear = tmp;
+    }
+}
+
+int queue_empty()
+{
+    if(front == NULL) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int dequeue()
+{
+    int vertex = front->vertex;
+ 
+    front = front->next;
+    return vertex;
+}
+void BFS(struct node **head, int num_vertices)
+{
+    int flag = 0;
+    int tmp_vertex = 0;
+    int i = 0;
+    struct node* tmp_node = NULL;
+
+    parent = (int*)malloc(num_vertices*sizeof(int));
+    color  = (int*)malloc(num_vertices*sizeof(int));
+
+
+    for(i = 0; i < num_vertices ; i++) {
+        color[i] = 0;
+        parent[i] = -9999;
+    }
+
+    parent[0] = -9999;
+    color[0] = 1;
+    enqueue(0);
+    while(!queue_empty()) {
+        tmp_vertex = dequeue();
+        printf("%d--", tmp_vertex);
+        tmp_node = head[tmp_vertex];
+
+        while(tmp_node) {
+            if(color[tmp_node->vertex] == 0) {
+                parent[tmp_node->vertex] = tmp_vertex;
+                color[tmp_node->vertex] = 1;
+                enqueue(tmp_node->vertex);
+            }
+            tmp_node = tmp_node->next;
+        }
+        color[head[tmp_vertex]->vertex] = 2;
+   
+    }
+}
+
+
+void push(int vertex)
+{
+    struct node* tmp = NULL;
+    tmp = (struct node*)malloc(sizeof(struct node));
+    tmp->vertex = vertex;
+    tmp->next = NULL;
+
+    if(top == NULL) {
+        top = tmp;
+    } else {
+        tmp->next = top;
+        top = tmp;
+    }
+}
+
+int pop()
+{
+    int vertex = top->vertex;
+    top = top->next;
+
+    return vertex;
+}
+
+int stack_empty()
+{
+    if(top == NULL) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void DFS(struct node **head, int num_vertices)
+{
+    int tmp_vertex = 0;
+    int i = 0;
+    struct node* tmp_node = NULL;
+
+    parent = (int*)malloc(num_vertices*sizeof(int));
+    color  = (int*)malloc(num_vertices*sizeof(int));
+
+
+    for(i = 0; i < num_vertices ; i++) {
+        color[i] = 0;
+        parent[i] = -9999;
+    }
+
+    parent[0] = -9999;
+    color[0] = 1;
+    push(0);
+    while(!stack_empty()) {
+        tmp_vertex = pop();
+        printf("%d--", tmp_vertex);
+        tmp_node = head[tmp_vertex];
+
+        while(tmp_node) {
+            if(color[tmp_node->vertex] == 0) {
+                parent[tmp_node->vertex] = tmp_vertex;
+                color[tmp_node->vertex] = 1;
+                push(tmp_node->vertex);
+            }
+            tmp_node = tmp_node->next;
+        }
+        color[head[tmp_vertex]->vertex] = 2;
+   
+    }
+}
