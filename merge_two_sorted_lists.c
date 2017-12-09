f0adb0c67a111b61c9e4aaf4275afb9afb369edd
vasistagithub <vasista.cse@gmail.com>
2017-12-09 12:58:27 +0530

    Changes to be committed: 	modified:   merge_two_sorted_lists.c

--- merge_two_sorted_lists.c
+++ merge_two_sorted_lists.c
@@ -10,6 +10,8 @@ void insert_list(struct ListNode**,int);
 void display_list(struct ListNode*);
 
 struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2);
+struct ListNode* mergeTwoLists_v2(struct ListNode* l1, struct ListNode* l2);
+void insert_list_v2(struct ListNode** ret_list, struct ListNode* list);
 
 int main()
 {
@@ -39,15 +41,25 @@ int main()
 
     printf("Displaying list-1\n");
     display_list(l1);
-    printf("Displaying list-2\n");
+    printf("\nDisplaying list-2\n");
     display_list(l2);
     
-    printf("Merging two sorted SLL\n");
+    printf("\nMerging two sorted SLL\n");
     l3 = mergeTwoLists(l1,l2);
     
     printf("Displaying list-3\n");
     display_list(l3);
+    
+    printf("\n");
 
+    printf("Merging two sorted SLL Version2\n");
+    l3 = mergeTwoLists_v2(l1,l2);
+    
+    printf("Displaying list-3\n");
+    display_list(l3);
+
+    printf("\n");
+    
     return 0;
 }
 
@@ -107,4 +119,58 @@ struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2)
     }
 
     return l3;
+}
+
+/* This function wont use extra memory for all nodes to create list3.
+ * Instead, it just links the pointers from list 1 and list2 to make
+ * a sorted list3 
+ */
+struct ListNode* mergeTwoLists_v2(struct ListNode* l1, struct ListNode* l2)
+{
+    struct ListNode* l3 = NULL;
+    struct ListNode* tmp_node = NULL;
+
+    while(l1 && l2) {
+        if(l1->val <= l2->val) {
+            tmp_node = l1->next;
+            insert_list_v2(&l3,l1);
+            l1 = tmp_node;
+        } else {
+            tmp_node = l2->next;
+            insert_list_v2(&l3,l2);
+            l2 = tmp_node;
+        }
+    }
+    if(l1 == NULL) {
+        while(l2) {
+            tmp_node = l2->next;
+            insert_list_v2(&l3, l2);
+            l2 = tmp_node;
+        }
+    }
+    if(l2 == NULL) {
+        while(l1) {
+            tmp_node = l1->next;
+            insert_list_v2(&l3,l1);
+            l1 = tmp_node;
+        }
+    }
+    return l3;
+}
+
+void insert_list_v2(struct ListNode** ret_list, struct ListNode* list)
+{
+    struct ListNode* tmp_node = NULL;
+
+    list->next = NULL;
+
+    if(*ret_list == NULL) {
+        *ret_list = list;
+    } else {
+        tmp_node = (*ret_list);
+        while(tmp_node->next) {
+            tmp_node = tmp_node->next;
+        }
+        tmp_node->next = list;
+    }
 }
